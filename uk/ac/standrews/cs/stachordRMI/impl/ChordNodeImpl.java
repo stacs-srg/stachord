e5cfdcefce4a7368a651e4b266bf426651c0d626
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2010-03-24 12:52:46 +0000

    *** empty log message ***

--- uk/ac/standrews/cs/stachordRMI/impl/ChordNodeImpl.java
+++ uk/ac/standrews/cs/stachordRMI/impl/ChordNodeImpl.java
@@ -17,7 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.standrews.cs.stachordRMI.impl;
-
+ 
 import java.net.InetSocketAddress;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
