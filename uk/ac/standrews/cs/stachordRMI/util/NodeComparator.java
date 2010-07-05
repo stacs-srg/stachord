b83d23a5a7b4b4710ff5d4a477e91932e0095b26
brb5 <devnull@localhost>
2010-07-05 20:59:48 +0000

    updated license headers (some files may have double headers now)

--- uk/ac/standrews/cs/stachordRMI/util/NodeComparator.java
+++ uk/ac/standrews/cs/stachordRMI/util/NodeComparator.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * StAChord Library
+ * Copyright (C) 2004-2008 Distributed Systems Architecture Research Group
+ * http://asa.cs.st-andrews.ac.uk/
+ * 
+ * This file is part of stachordRMI.
+ * 
+ * stachordRMI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * stachordRMI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with stachordRMI.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package uk.ac.standrews.cs.stachordRMI.util;
 
 import java.io.Serializable;
