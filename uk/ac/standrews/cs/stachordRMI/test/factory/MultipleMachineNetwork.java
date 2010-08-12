a115166d4dfe57e6de832be67d78f120a2e33870
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2010-08-12 14:34:46 +0000

    Multiple changes.

--- uk/ac/standrews/cs/stachordRMI/test/factory/MultipleMachineNetwork.java
+++ uk/ac/standrews/cs/stachordRMI/test/factory/MultipleMachineNetwork.java
@@ -20,10 +20,6 @@
  ******************************************************************************/
 package uk.ac.standrews.cs.stachordRMI.test.factory;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
 import java.math.BigInteger;
 import java.rmi.NotBoundException;
@@ -56,53 +52,62 @@ import uk.ac.standrews.cs.stachordRMI.util.NodeComparator;
 
 import com.mindbright.ssh2.SSH2Exception;
 
+/**
+ * Network comprising Chord nodes running on a set of specified physical machines running Linux or OSX.
+ * 
+ * @author Alan Dearle (al@cs.st-andrews.ac.uk)
+ * @author Graham Kirby(graham@cs.st-andrews.ac.uk)
+ */
 public class MultipleMachineNetwork implements INetwork {
 	
-	protected interface ArgGen {
-			List<String> getArgs(int local_port) throws RemoteException;
-		}
-
-	public static final String RANDOM =    "RANDOM";       // Nodes randomly distributed around the ring.
-	public static final String EVEN =      "EVEN";         // Nodes evenly distributed around the ring.
-	public static final String CLUSTERED = "CLUSTERED";    // Nodes clustered tightly in one region of the ring.
-
-	static int next_port = 54496;
-	static final Object sync = new Object();
-	private static final int REGISTRY_RETRY_INTERVAL = 2000;
-	private static final int REGISTRY_TIMEOUT_INTERVAL = 20000;
+	private static int next_port = 54496;                         // The next port to be used; static to allow multiple concurrent networks.
+	private static final Object sync = new Object();              // Used for serializing network creation.
 	
-	private static Random random = new Random(45974927);
+	private static final int REGISTRY_RETRY_INTERVAL =    2000;   // Retry connecting to remote nodes at 2s intervals.
+	private static final int REGISTRY_TIMEOUT_INTERVAL = 20000;   // Give up after 20s.
 	
-	IKey[] node_keys;
-	SortedSet<IChordRemoteReference> nodes;
+	private static Random random = new Random(45974927);          // Used to randomize the point of joining a ring.
+	
+	private IKey[] node_keys;                                     // The keys of the nodes.
+	private TreeSet<IChordRemoteReference> nodes;                 // The nodes themselves.
 
-	protected Map<IChordRemoteReference, Process> process_table;
+	protected Map<IChordRemoteReference, Process> process_table;  // Map to keep track of the process handle for each node.
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Disable arg-less constructor.
+	 */
 	protected MultipleMachineNetwork() {
 	}
 
-	public MultipleMachineNetwork(NodeDescriptor[] node_descriptors, String network_type) throws IOException, NotBoundException, SSH2Exception {
+	/**
+	 * @param node_descriptors a description of the target physical host for each Chord node to be created
+	 * @param key_distribution the required key distribution
+	 * 
+	 * @throws IOException if the process for a node cannot be created
+	 * @throws SSH2Exception if communication with a remote host fails
+	 */
+	public MultipleMachineNetwork(NodeDescriptor[] node_descriptors, KeyDistribution key_distribution) throws IOException, SSH2Exception {
 		
-		init(node_descriptors, network_type);
+		init(node_descriptors, key_distribution);
 	}
 
-	protected void init(NodeDescriptor[] node_descriptors, String network_type) throws IOException, NotBoundException, SSH2Exception {
+	protected void init(NodeDescriptor[] node_descriptors, KeyDistribution key_distribution) throws IOException, SSH2Exception {
 		
 		process_table = new HashMap<IChordRemoteReference, Process>();
 		
 		// Serialize creations of networks, since they update static field 'next_port'.
 		synchronized (sync) {
 			
-			if (!network_type.equals(RANDOM) && !network_type.equals(EVEN) && !network_type.equals(CLUSTERED)) fail("unknown network type");
-	
-			node_keys = generateNodeKeys(network_type, node_descriptors.length);
+			node_keys = generateNodeKeys(key_distribution, node_descriptors.length);
 			nodes = new TreeSet<IChordRemoteReference>(new NodeComparator());
 			
 			IChordRemoteReference first = createFirstNode(node_descriptors[0], node_keys[0]);
 			nodes.add(first);
 		
 			for (int node_index = 1; node_index < node_descriptors.length; node_index++) {
-
+		
 				IChordRemoteReference known_node = pickRandomElement(nodes);
 				IKey key = node_keys[node_index];
 		
@@ -112,16 +117,59 @@ public class MultipleMachineNetwork implements INetwork {
 		}
 	}
 
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@SuppressWarnings("unchecked")
 	public SortedSet<IChordRemoteReference> getNodes() {
 		
-		return nodes;
+		// Clone set to prevent caller changing contents.
+		return (SortedSet<IChordRemoteReference>) nodes.clone();
 	}
+
+	public void killNode(IChordRemoteReference node) {
+		
+		synchronized (nodes) {
+			
+			if (nodes.contains(node)) {
+
+				int network_size = nodes.size();
+
+				process_table.get(node).destroy();
+				process_table.remove(node);
+
+				boolean successfully_removed = nodes.remove(node);
+				assert successfully_removed;
+
+				assert nodes.size() == network_size - 1;
+			}
+		}
+	}
+
+	public void killAllNodes() {
+		
+		synchronized (nodes) {
+			
+			for (IChordRemoteReference node : getNodes()) {
+				
+				process_table.get(node).destroy();
+				process_table.remove(node);
+			}
+			nodes.clear();
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	private IChordRemoteReference pickRandomElement(SortedSet<IChordRemoteReference> nodes) {
 		
 		int index = randomIndex(0, nodes.size());
 		int count = 0;
 		
+		// Iterate through set since no way to pick element with specified index.
 		for (IChordRemoteReference reference : nodes) {
 			if (count++ == index) return reference;
 		}
@@ -134,7 +182,11 @@ public class MultipleMachineNetwork implements INetwork {
 		return node_descriptor.ssh_client_wrapper.getServer().getHostName();
 	}
 
-	protected IChordRemoteReference createFirstNode(final NodeDescriptor node_descriptor, final IKey key) throws IOException, NotBoundException, SSH2Exception {
+	private interface ArgGen {
+		List<String> getArgs(int local_port);
+	}
+
+	private IChordRemoteReference createFirstNode(final NodeDescriptor node_descriptor, final IKey key) throws IOException, SSH2Exception {
 		
 		ArgGen arg_gen = new ArgGen() {
 			
@@ -152,16 +204,16 @@ public class MultipleMachineNetwork implements INetwork {
 		return createNode(node_descriptor, arg_gen, StartRing.class);
 	}
 
-	protected IChordRemoteReference createJoiningNode(final NodeDescriptor node_descriptor, final IChordRemoteReference known_node, final IKey key) throws IOException, NotBoundException, SSH2Exception {
+	private IChordRemoteReference createJoiningNode(final NodeDescriptor node_descriptor, final IChordRemoteReference known_node, final IKey key) throws IOException, SSH2Exception {
 		
 		ArgGen arg_gen = new ArgGen() {
 			
-			public List<String> getArgs(int local_port) throws RemoteException {
+			public List<String> getArgs(int local_port) {
 				
 				List<String> args = new ArrayList<String>();
 				
 				args.add("-s" + NetworkUtil.formatHostAddress(getHost(node_descriptor), local_port));
-				args.add("-k" + NetworkUtil.formatHostAddress(known_node.getRemote().getAddress())); 
+				args.add("-k" + NetworkUtil.formatHostAddress(known_node.getAddress())); 
 				addKeyArg(key, args);
 				
 				return args;
@@ -172,28 +224,28 @@ public class MultipleMachineNetwork implements INetwork {
 	}
 
 	// Generates a random index such that lower <= index < upper.
-	protected int randomIndex(int lower, int upper) {
+	private int randomIndex(int lower, int upper) {
 		
 		return lower + random.nextInt(upper - lower);
 	}
 	
-	protected IKey[] generateNodeKeys(String network_type, int number_of_nodes) {
+	private IKey[] generateNodeKeys(KeyDistribution network_type, int number_of_nodes) {
 		
 		IKey[] node_keys = new IKey[number_of_nodes];
 		
 		// Leave null entries for RANDOM since the keys will be generated from hashing addresses.
-		if (!network_type.equals(RANDOM)) {
+		if (network_type != KeyDistribution.RANDOM) {
 		
 			BigInteger key_value = null;
 			BigInteger node_increment = null;
 	
-			if (network_type.equals(EVEN)) {
+			if (network_type == KeyDistribution.EVEN) {
 				
 				key_value = new BigInteger("0");
 				node_increment = Key.KEYSPACE_SIZE.divide(new BigInteger(String.valueOf(number_of_nodes)));	
 			}
 			
-			if (network_type.equals(CLUSTERED)) {
+			if (network_type == KeyDistribution.CLUSTERED) {
 				
 				// Span the wrap-over point just to be awkward.
 				key_value = Key.KEYSPACE_SIZE.subtract(new BigInteger(String.valueOf(number_of_nodes / 2)));
@@ -209,12 +261,12 @@ public class MultipleMachineNetwork implements INetwork {
 		return node_keys;
 	}
 
-	protected void addKeyArg(IKey key, List<String> args) {
+	private void addKeyArg(IKey key, List<String> args) {
 	
 		if (key != null) args.add("-x" + key.toString(Key.DEFAULT_RADIX)); 
 	}
 
-	protected IChordRemoteReference bindToNode(String host, int port) throws TimeoutException {
+	private IChordRemoteReference bindToNode(String host, int port) throws TimeoutException {
 				
 		long start_time = System.currentTimeMillis();
 		
@@ -248,31 +300,6 @@ public class MultipleMachineNetwork implements INetwork {
 		}
 	}
 
-	public void killNode(IChordRemoteReference node) {
-	
-		synchronized (nodes) {
-			
-			int network_size = nodes.size();
-			assertTrue(nodes.contains(node));
-	
-			process_table.get(node).destroy();
-			
-			assertTrue(nodes.contains(node));
-			assertTrue(nodes.remove(node));
-			assertEquals(nodes.size(), network_size - 1);
-		}
-	}
-
-	public void killAllNodes() {
-		
-		synchronized (nodes) {
-			for (IChordRemoteReference node : getNodes()) {
-				process_table.get(node).destroy();
-			}
-			nodes.clear();
-		}
-	}
-
 	private IChordRemoteReference createNode(NodeDescriptor node_descriptor, ArgGen arg_gen, Class<? extends AbstractServer> node_class) throws IOException, SSH2Exception {
 		
 		boolean finished = false;
@@ -301,6 +328,11 @@ public class MultipleMachineNetwork implements INetwork {
 
 	protected Process runProcess(NodeDescriptor node_descriptor, Class<? extends AbstractServer> node_class, List<String> args) throws IOException, SSH2Exception {
 		
-		return Processes.runJavaProcess(node_class, args, node_descriptor.ssh_client_wrapper, node_descriptor.java_version, node_descriptor.class_path);
+		if (node_descriptor.lib_urls != null) {
+			return Processes.runJavaProcess(node_class, args, node_descriptor.ssh_client_wrapper, node_descriptor.java_version, node_descriptor.lib_urls, node_descriptor.wget_path, node_descriptor.lib_install_dir, true);
+		}
+		else {
+			return Processes.runJavaProcess(node_class, args, node_descriptor.ssh_client_wrapper, node_descriptor.java_version, node_descriptor.class_path);
+		}
 	}
 }
