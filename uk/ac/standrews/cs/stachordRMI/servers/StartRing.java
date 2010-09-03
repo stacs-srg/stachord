db91db20f7a38fd002e96e7e233d8b7933051187
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2010-09-03 15:01:45 +0000

    *** empty log message ***

--- uk/ac/standrews/cs/stachordRMI/servers/StartRing.java
+++ uk/ac/standrews/cs/stachordRMI/servers/StartRing.java
@@ -61,5 +61,6 @@ public class StartRing extends AbstractServer {
 			Diagnostic.trace(DiagnosticLevel.FULL, "Failed to start new RMI Chord ring: " + e.getMessage());
 		}
 		System.out.println("exiting main");
+		System.exit(0);
 	}
 }
