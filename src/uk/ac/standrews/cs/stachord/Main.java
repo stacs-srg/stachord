3d24b09fd076152ce1fe8b3c8895f663678741b9
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2011-01-20 21:12:36 +0000

    Tidying.

---
+++ src/uk/ac/standrews/cs/stachord/Main.java
@@ -0,0 +1,14 @@
+package uk.ac.standrews.cs.stachord;
+
+public class Main {
+
+    /**
+     * For testing release.
+     *
+     * @param args arguments
+     */
+    public static void main(final String[] args) {
+
+        System.out.println("Installed successfully");
+    }
+}
