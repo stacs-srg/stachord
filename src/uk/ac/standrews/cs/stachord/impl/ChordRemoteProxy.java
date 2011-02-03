2af4a5cf697d309972bf49713589c055df8d065a
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2011-02-03 14:27:25 +0000

    Debugging recovery tests.

--- src/uk/ac/standrews/cs/stachord/impl/ChordRemoteProxy.java
+++ src/uk/ac/standrews/cs/stachord/impl/ChordRemoteProxy.java
@@ -223,9 +223,9 @@ public final class ChordRemoteProxy extends Proxy implements IChordRemote {
     public void isAlive() throws RPCException {
 
         try {
-            debug = true;
+            //            debug = true;
             makeCall("isAlive");
-            debug = false;
+            //            debug = false;
         }
         catch (final Exception e) {
             dealWithException(e);
