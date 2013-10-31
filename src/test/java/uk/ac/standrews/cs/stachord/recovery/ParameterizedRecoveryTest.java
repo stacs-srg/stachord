fab0ea109507f3715b032c36cc7a6382605a93f2
Masih Hajiarabderkani <masih.h.derkani@st-andrews.ac.uk>
2013-10-31 16:22:36 +0000

    disabled all test parallelizations

--- src/test/java/uk/ac/standrews/cs/stachord/recovery/ParameterizedRecoveryTest.java
+++ src/test/java/uk/ac/standrews/cs/stachord/recovery/ParameterizedRecoveryTest.java
@@ -26,9 +26,7 @@
 package uk.ac.standrews.cs.stachord.recovery;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
@@ -42,9 +40,6 @@ import uk.ac.standrews.cs.nds.util.Diagnostic;
 import uk.ac.standrews.cs.nds.util.DiagnosticLevel;
 import uk.ac.standrews.cs.nds.util.Duration;
 import uk.ac.standrews.cs.shabdiz.ApplicationState;
-import uk.ac.standrews.cs.shabdiz.host.Host;
-import uk.ac.standrews.cs.shabdiz.host.LocalHost;
-import uk.ac.standrews.cs.shabdiz.testing.junit.ParallelParameterized;
 import uk.ac.standrews.cs.shabdiz.util.Combinations;
 
 /**
@@ -53,12 +48,11 @@ import uk.ac.standrews.cs.shabdiz.util.Combinations;
  *
  * @author Masih Hajiarabderkani (mh638@st-andrews.ac.uk)
  */
-@RunWith(ParallelParameterized.class)
-@ParallelParameterized.Parallelization(threadCount = 10, hostProvider = "local", jvmArguments = "-Xmx128m")
+@RunWith(Parameterized.class)
 public abstract class ParameterizedRecoveryTest {
 
-    protected static final Integer[] RING_SIZES = {1, 2, 3, 5, 10};
-    protected static final KeyDistribution[] KEY_DISTRIBUTIONS = {KeyDistribution.RANDOM, KeyDistribution.EVEN, KeyDistribution.CLUSTERED};
+    protected static final Integer[] RING_SIZES = {10};
+    protected static final KeyDistribution[] KEY_DISTRIBUTIONS = {KeyDistribution.RANDOM, KeyDistribution.CLUSTERED, KeyDistribution.EVEN};
     private static final Duration CHECK_TIMEOUT = new Duration(10, TimeUnit.MINUTES); // Allow 10 minutes for each check operation.
     private final ChordNetwork network;
     private final int ring_size;
@@ -71,15 +65,6 @@ public abstract class ParameterizedRecoveryTest {
         this.ring_size = ring_size;
         this.key_distribution = key_distribution;
         network = createNetwork(ring_size, key_distribution);
-    }
-
-    @ParallelParameterized.HostProvider(name = "local")
-    public static List<Host> getLocalHost() throws IOException {
-
-        final List<Host> localhost = new ArrayList<Host>();
-        localhost.add(new LocalHost());
-        return localhost;
-
     }
 
     @Parameterized.Parameters(name = "{index} -  ring size:{0}, key distribution: {1}")
